2fffa866efce75e006de223eb8fa012e782adb25
Rizwan Hasan <Rizzystar21@gmail.com>
2024-01-05 00:56:27 -0600

    Add project files.

---
+++ Blackjack2022/Blackjack.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack2022
+{
+    class Blackjack
+    {
+        public List<Card> shoe = new List<Card>();
+        public List<Card> playerDeck = new List<Card>();
+        public List<Card> dealerDeck = new List<Card>();
+
+        public void GiveToPlayer()
+        {
+            DealTo(playerDeck);
+        }
+        public void GiveToDealer()
+        {
+            DealTo(dealerDeck);
+        }
+        private void DealTo(List<Card> deck)
+        {
+            if (shoe.Count < 1)
+            {
+                throw new Exception("Trying to deal from empty shoe");
+            }
+
+            Card c = shoe[0];
+            deck.Add(c);
+            shoe.RemoveAt(0);
+        }
+        private void ClearDeal(List<Card> deck)
+        {
+            deck.Clear();
+            playerDeck.Clear();
+            dealerDeck.Clear();
+        }
+
+        public void Clear()
+        {
+            ClearDeal(shoe);
+            InitShoe();
+        }
+        public void InitShoe()
+        {
+            string[] suits = new string[] { "Hearts", "Diamonds", "Clubs", "Spades" };
+
+            // Create 52 cards and place them into the shoe/dispenser
+            for (int k = 0; k < 4; k++)
+            {
+                for (int i = 1; i < 14; i++)
+                {
+                    Card c = new Card();
+                    c.Suit = suits[k];
+                    c.Rank = i;
+                    shoe.Add(c);
+                }
+            }
+
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            Random rand = new Random();
+
+            for(int i = 0; i < shoe.Count; i++)
+            {
+                int index = rand.Next(51);
+                Card card = shoe[index];
+                shoe[index] = shoe[i];
+                shoe[i] = card;
+            }
+        }
+
+        public void Start()
+        {
+            GiveToPlayer();
+            GiveToDealer();
+            GiveToPlayer();
+            GiveToDealer();
+        }
+
+        public int Hit()
+        {
+            GiveToPlayer();
+
+            if(GetPlayerSum() > 21)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public int Stand() {
+
+            GiveToDealer();
+            if (GetDealerSum() < GetPlayerSum() || GetDealerSum() > 21) { 
+                return -1;
+            }
+            return 0;
+        }
+
+        public int GetPlayerSum()
+        {
+            int sum = 0;
+            foreach(Card c in playerDeck)
+            {
+                sum += c.GetBlackjackValue();  // Naive
+            }
+
+            return sum;
+        }
+        public int GetDealerSum()
+        {
+            int sum = 0;
+            foreach (Card c in dealerDeck)
+            {
+                sum += c.GetBlackjackValue();  // Naive
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Blackjack2022/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Blackjack2022
+{
+    public partial class MainWindow : Window
+    {
+        Blackjack game = new Blackjack();
+        public MainWindow()
+        {
+            InitializeComponent();
+            game.InitShoe();
+        }
+        
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            game.Start();
+            RefreshScreen();
+            StartButton.IsEnabled = false;
+            HitButton.IsEnabled = true;
+            StandButton.IsEnabled = true;
+            RestartButton.Visibility = Visibility.Hidden;
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            game.Clear();
+            StartButton.IsEnabled = true;
+            HitButton.IsEnabled = true;
+            StandButton.IsEnabled = true;
+            PlayerPanel.Children.Clear();
+            DealerPanel.Children.Clear();
+            
+        }
+        private void HitButton_Click(object sender, RoutedEventArgs e)
+        {
+            int result = game.Hit();
+            RefreshScreen();
+            if(result < 0)
+            {
+                MessageBox.Show("You Lost :(");
+                RestartButton.Visibility = Visibility.Visible;
+                HitButton.IsEnabled = false;
+            }
+        }
+
+        private void StandButton_Click(object sender, RoutedEventArgs e)
+        {
+            int dealerResult = game.Stand();
+            RefreshScreen();
+            if (dealerResult < 0)
+            {
+                MessageBox.Show("You Win! :)");
+                RestartButton.Visibility = Visibility.Visible;
+                HitButton.IsEnabled = false;
+                StandButton.IsEnabled = false;
+
+            }
+            else
+                MessageBox.Show("You Lost :(");
+                RestartButton.Visibility = Visibility.Visible;
+                HitButton.IsEnabled = false;
+                StandButton.IsEnabled = false;
+        }
+
+        public void RefreshScreen()
+        {
+            PlayerPanel.Children.Clear();
+            DealerPanel.Children.Clear();
+            foreach (Card c in game.playerDeck)
+            {
+                ShowCard(c, PlayerPanel);
+            }
+            foreach (Card c in game.dealerDeck)
+            {
+                ShowCard(c, DealerPanel);
+            }
+        }
+
+        public void ShowCard(Card c, Panel panel)
+        {
+            string path = "/png/";
+            string filename = c.GetFileName();
+
+            Uri uri = new Uri(path + filename, UriKind.Relative);
+            BitmapImage bitmapImage = new BitmapImage(uri);
+            Image image = new Image();
+            image.Source = bitmapImage;
+
+            panel.Children.Add(image);
+        }
+        public void ClearCards(List<Card> deck, Panel panel)
+        {
+            deck.Clear();
+            PlayerPanel.Children.Clear();
+            DealerPanel.Children.Clear();  
+            
+        }
+    }
+}
